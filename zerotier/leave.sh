6d5059d68f7a3f260ce087096a4bf95ea83ef557
eventlOwOp <3064273390@qq.com>
2024-05-06 21:01:47 +0800

    keep up with main

--- zerotier/leave.sh
+++ zerotier/leave.sh
@@ -1,2 +1,3 @@
-export LD_LIBRARY_PATH=/data/adb/zerotier
-cat ./network | xargs ./zerotier-cli -D./home leave
+export LD_LIBRARY_PATH=/data/adb/zerotier/lib
+cd /data/adb/zerotier
+cat /sdcard/Android/zerotier/network_id.txt | xargs ./zerotier-cli -D./home leave
